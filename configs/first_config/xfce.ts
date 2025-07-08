45af184d8a1d900511a2a215b5ca17ccfa48309b
Timm Nicolaizik <timmmmnn@gmail.com>
2025-07-08 16:28:25 +0200

    Updated xfce config

--- configs/first_config/xfce.ts
+++ configs/first_config/xfce.ts
@@ -4,6 +4,21 @@ export default [
     groupInstall: "all",
     postInstall: "sudo systemctl enable lightdm.service",
   },
+
+  {
+    name: "xfce4",
+    groupInstall: "all",
+  },
+
+  {
+    name: "xfce4-goodies",
+    groupInstall: "all",
+  },
+
+  {
+    name: "lightdm-gtk-greeter",
+  },
+
   {
     name: "extra/lightdm",
     postInstall: "sudo systemctl enable lightdm.service",
