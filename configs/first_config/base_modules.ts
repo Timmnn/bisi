a54550df3e560720f55d5862604e5cd5290c2bc2
Timm Nicolaizik <timmmmnn@gmail.com>
2025-07-08 17:08:05 +0000

    Changed config

--- configs/first_config/base_modules.ts
+++ configs/first_config/base_modules.ts
@@ -24,6 +24,7 @@ export default [
     // Runs just once after instllation
     postInstall: `git config --global user.name 'Timm Nicolaizik'; 
         git config --global user.email 'timmmmnn@gmail.com'; 
+        git config --global core.editro 'nvim'; 
         git config --global credential.helper store`,
   },
 ];
