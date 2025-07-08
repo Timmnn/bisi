33f9e1cf8e734728778aac1568cce99965466f72
Timm Nicolaizik <timmmmnn@gmail.com>
2025-07-08 16:25:09 +0200

    Updated config

--- configs/first_config/xfce.ts
+++ configs/first_config/xfce.ts
@@ -1,13 +1,11 @@
 export default [
   {
-    name: "extra/lightdm",
-    postInstall: "sudo systemctl enable lightdm.service",
-  },
-
-  "extra/lightdm",
-  {
     name: "xorg",
     groupInstall: "all",
+    postInstall: "sudo systemctl enable lightdm.service",
+  },
+  {
+    name: "extra/lightdm",
     postInstall: "sudo systemctl enable lightdm.service",
   },
 ];
